5ba100afa12e9a2277257919abfe9d1a70d8b856
bobtimusprime <thebobmister54@gmail.com>
2017-11-16 21:15:59 +0000

    Added delete and search routes

--- models/Students.js
+++ models/Students.js
@@ -3,5 +3,10 @@ var StudentSchema = new mongoose.Schema({
   name:String,
   practiceTime:{type:Number, default:0}
 });
+
+StudentSchema.methods.addPracticeTime = function(cb) {
+  this.practiceTime +=1;
+  this.save(cb);
+};
 mongoose.model('Student', StudentSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Student  = mongoose.model('Student');
 
+router.param('student', function(req, res, next, id) {
+  var query = Student.findById(id);
+  query.exec(function(err, comment) {
+    if (err) { return next(err) }
+    if (!comment) { return next( new Error("Can't find comment"));}
+    req.comment = comment;
+    return next();
+  });
+});
+
 router.post('/students', function(req, res,next){
   var student = new Student(req.body);
   student.save(function(err, student){
@@ -16,6 +26,26 @@ router.get('/students', function(req, res, next) {
     if(err){ return next(err); }
     res.json(students);
   });
+});
+
+
+router.get('/students/:student', function(req, res) {
+  res.json(req.student);
+});
+
+//req.student is the object found in the param route
+//addPracticeTime is the method declared with the schema
+router.put('/students/:student/practiceTime', function(req, res, next) {
+  req.student.addPracticeTime(function(err, student){
+    if (err) { return next(err);}
+    res.json(student);
+  });
+});
+
+router.delete('/students/:student', function(req, res) {
+  console.log("in Delete");
+  req.student.remove();
+  res.sendStatus(200);
 });
 
 module.exports = router;
